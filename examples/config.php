80ed72814370bafe63f06760169f12fcdc8ef9de
soif <wxopwx@gmail.com>
2018-02-04 18:36:51 +0100

    Examples : Header Builder + add jQuery

--- examples/config.php
+++ examples/config.php
@@ -12,11 +12,13 @@ $cfg['html_head']=<<<EOF
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> 
 	<!-- BootStrap -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-	<title>phpDMX Fixtures</title>
+	<!--HEAD_CONTENT-->
+
 <style>
 .att_table{
 	border: 1px solid #ccc;
@@ -54,6 +56,13 @@ EOF;
 
 
 // UTILS #############################################################################################################################################################
+function HtmlHead($title='',$extras=''){
+	global $cfg;
+	$title and $head="<title>$title</title>\n";
+	$head .=$extras;
+	return str_replace('<!--HEAD_CONTENT-->',$head, $cfg['html_head']);	
+}
+
 // ------------------------------------------------------------------------
 function AttributeToHtml($obj, $att, $name=''){
 	$value=$obj->$att;
